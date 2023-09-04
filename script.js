8f04f0ff8c1f94dd4d20d0a15a29e889307391a7
AlhsanAlmajed <142681748+AlhsanAlmajed@users.noreply.github.com>
2023-09-04 19:34:25 +0300

    .

---
+++ script.js
@@ -0,0 +1,34 @@
+alert ("مرحبا بك في موقعي ! انا الحسن , وسوف احسب درجتك");
+let grade = prompt("ادحل درجاتك");
+console.log (grade)
+if (grade >=  90 && grade <=100){
+    
+    console.log("امتياز");
+    
+}
+    
+    else if (grade >= 80 && grade <= 89 )
+    {
+console.log ("جيد جدا");
+}
+
+else if ( grade >= 70 && grade <= 79 ){
+
+    console.log ("جيد");
+}
+
+else if ( grade >= 60 && grade <= 69 ){
+
+    console.log ("مقبول");
+}
+
+else if ( grade >= 50 && grade <= 59 ){
+
+    console.log ("ضعيف");
+}
+
+else if ( grade <= 50 ){
+
+    console.log ("ضعيف");
+}
+
